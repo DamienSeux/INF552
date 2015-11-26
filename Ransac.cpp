36aa43afee4b69d4f33aecf085c755e50b0bf4dd
DamienSeux <d.seux3@gmail.com>
2015-11-26 18:24:51 +0100

    ...

--- Ransac.cpp
+++ Ransac.cpp
@@ -1,21 +1,13 @@
-#include <stdexcept>
-#include <vector>
-#include <cmath>
 
-using namespace std;
 
-template <typename pt_type, typename F,int min>
 /* n is the number of component for the data, pt_type their type (float of int), n the min number of point for the model
- F is the class of the model
- F should have :
- - a min field : the minimum number of point to fit the model (static)
- - an init function fitting the model to the min first data points
- - a inlier function counting the number of inliers
+ F is the class of the model, whose parameters are computed when constructor is called.
 
  This is the RANSAC explain in class and used in open CV and not the usual version :
  the least square fit is only applied to the best model and there is no minimum number
  of inliers.
 */
+template <typename pt_type, typename dt_type, typename F,int min>
 class RANSAC{
 private:
     bool inliers[];
@@ -23,11 +15,11 @@ private:
     int error; // score of the best model : the number of inliers
     pt_type thres; // thresold to decide who is an insider
     int nstep; // how many random sample we try
-    vector<pair<vector<pt_type>,vector<pt_type> > > data; // the data, is points of n components of type pt_type.
+    vector<pair<dt_type,dt_type > > data; // the data, is points of n components of type pt_type.
     vector<int> ranks;
 
 public :
-    RANSAC(const vector<pair<vector<pt_type>,vector<pt_type> > >& data,pt_type thres,int nstep):data(data),nstep(nstep),thres(thres){
+    RANSAC(const vector<pair<dt_type,dt_type> >& data,pt_type thres,int nstep):data(data),nstep(nstep),thres(thres){
         if(data.size()<min){
             throw invalid_argument("Pas assez de points");
         }
@@ -38,13 +30,13 @@ public :
         }
     };
 
-    void copy(bool src[],bool dst[],int size){
+    inline void copy(bool src[],bool dst[],int size){
         for(int i=0;i<size;i++){
             dst[i]=src[i];
         }
     }
 
-    void randomize(vector<int>& t){
+    inline void randomize(vector<int>& t){
         //sample min points at position 0..min-1 in t by swapping.
         srand((uint) time(NULL));
         if(min>t.size()){
@@ -59,7 +51,7 @@ public :
         }
     }
 
-    void compute(){
+    inline void compute(){
         best=NULL;
         error=NULL;
         for(int i=0;i<nstep;i++){
@@ -81,82 +73,26 @@ public :
     inline bool* get_inliers(){return inliers;}
 
 };
-template <typename pt_type>
-struct Droite{
-    pt_type param[3];
-    bool inliers[];
-    int error;
 
-    void normalize(pt_type param[3] ){
-        float norm=sqrt(param[0]*param[0]+param[1]*param[1]+param[2]*param[2]);
-        if(norm>0){
-            param[0]=param[0]/norm;
-            param[1]=param[1]/norm;
-            param[2]=param[2]/norm;
-        }
-    }
-
-    Droite(const vector<pair<pt_type,pt_type> >& data,const vector<int>& ranks,pt_type thres):inliers(new bool[data.size()]){
-        pair<pt_type,pt_type> point1=data[ranks[0]];
-        pair<pt_type,pt_type> point2=data[ranks[1]];
-        if(point1==point2){
-            param[0]=0;
-            param[1]=0;
-            param[2]=0;
-            error=data.size();
-            for(int i=0;i<data.size();i++){
-                inliers[i]=false;
-            }
-            return;
-        }
-
-        if(point1.first==point2.first){
-            param[0]=0;
-            param[1]=1;
-            param[2]=-1*point1.first;
-            error=0;
-            bool flag;
-            for(int i=0;i<data.size();i++){
-                flag=(data[i].first+param[2]<thres);
-                inliers[i]=flag;
-                if(!flag)
-                    error++;
-            }
-            return;
-        }else{
-            param[0]=(point2.second-point1.second);
-            param[1]=(point1.first-point2.first);
-            param[2]=-1*(param[0]*point1.first+param[1]*point1.second);
-            normalize(param);
-            pair<pt_type,pt_type> origin=pair<pt_type,pt_type>(0,-param[2]/param[1]);
-            bool flag;
-            error=0;
-            for(int i=0;i<data.size();i++){
-                flag=(data[i].first-origin.first)*param[0]+(data[i].second-origin.second)*param[1]<thres;
-                inliers[i]=flag;
-                if(!flag)
-                    error++;
-            }
-          return;
-        }
-
-    }
-    Droite(const Droite<pt_type>& d):inliers(d.inliers),error(d.error){
-        pt_type param[2];
-        param[0]=d.param[0];
-        param[1]=d.param[1];
-    }
-    inline bool* get_inliers() const{
-        return inliers;
-    }
-    inline int get_error() const{
-        return error;
-    }
-
-
-};
 
 int main(int argc, char *argv[]){
+
+    vector<pair<float,float> > data;
+    srand((uint) time(NULL));
+    for(int i=0;i<200;i++){
+        data.push_back(pair<float,float>(i,5*i+12+(rand() % (100)-50)/(float) 100));
+    }
+    RANSAC<float,float,Droite,2> test(data,2,100);
+
+    test.compute();
+
+    float* param=test.get_best().param;
+    cout<<param[0]<<","<<param[1]<<param[2]<<endl;
+    cout<<test.get_best().get_error();
+
+
+
+
 
     return 0;
 }
