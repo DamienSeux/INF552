c451cee1e476fc4ca28ada8ace6a5d9a4924061c
DamienSeux <d.seux3@gmail.com>
2015-11-26 19:09:06 +0100

    Test de la droite pas mal

--- Ransac.cpp
+++ Ransac.cpp
@@ -1,4 +1,7 @@
-
+#include <stdexcept>
+#include <vector>
+#include <iostream>
+#include "Droite.cpp"
 
 /* n is the number of component for the data, pt_type their type (float of int), n the min number of point for the model
  F is the class of the model, whose parameters are computed when constructor is called.
@@ -13,18 +16,18 @@ private:
     bool inliers[];
     F best; // the best model so far, or null if no model has been found
     int error; // score of the best model : the number of inliers
-    pt_type thres; // thresold to decide who is an insider
+    float thres; // thresold to decide who is an insider
     int nstep; // how many random sample we try
     vector<pair<dt_type,dt_type > > data; // the data, is points of n components of type pt_type.
     vector<int> ranks;
 
 public :
-    RANSAC(const vector<pair<dt_type,dt_type> >& data,pt_type thres,int nstep):data(data),nstep(nstep),thres(thres){
+    RANSAC(const vector<pair<dt_type,dt_type> >& data,float thres,int nstep):data(data),nstep(nstep),thres(thres){
         if(data.size()<min){
             throw invalid_argument("Pas assez de points");
         }
         inliers[data.size()];
-        vector<int> ranks;
+        ranks=vector<int>();
         for(int i=0;i<data.size();i++){
             ranks.push_back(i);
         }
@@ -52,17 +55,17 @@ public :
     }
 
     inline void compute(){
-        best=NULL;
-        error=NULL;
+        best=F();
+        error=data.size();
         for(int i=0;i<nstep;i++){
             randomize(ranks);
-            F model(data,ranks);
+            F model(data,ranks,thres);
             if(model.get_error()<error){
                 best=model;
                 error=model.get_error();
-                copy(model.get_inliers(),inliers,data.size());
+                //copy(model.get_inliers(),inliers,data.size());
             }
-            delete model;
+            //delete model;
         }
 
 
@@ -79,15 +82,20 @@ int main(int argc, char *argv[]){
 
     vector<pair<float,float> > data;
     srand((uint) time(NULL));
-    for(int i=0;i<200;i++){
-        data.push_back(pair<float,float>(i,5*i+12+(rand() % (100)-50)/(float) 100));
+    for(int i=0;i<1000;i++){
+        if(rand()%10!=0) {
+            data.push_back(pair<float, float>(5 *(i/10), 25 * (i/10) + 12 + (rand() % (100) - 50) / (float) 10));
+        }
+        else{
+            data.push_back(pair<float,float>(12*(i/10),14*(i/10)+78+(rand() % (100) - 50) / (float) 30));
+        }
     }
-    RANSAC<float,float,Droite,2> test(data,2,100);
+    RANSAC<float,float,Droite<float>,2> test=RANSAC<float,float,Droite<float>,2>(data,0.2,10000);
 
     test.compute();
 
     float* param=test.get_best().param;
-    cout<<param[0]<<","<<param[1]<<param[2]<<endl;
+    cout<<param[0]<<","<<param[1]<<","<<param[2]<<endl;
     cout<<test.get_best().get_error();
 
 
